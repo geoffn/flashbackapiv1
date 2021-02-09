98c960683c52d44ce766e713c45b1caddf37e5ae
Geoff Nelson <geoffreysnelson@gmail.com>
2021-02-09 13:28:20 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+require('./src/db/mongoose')
+const express = require("express")
+const cors = require('cors')
+
+const app = express();
+
+//Import our user router and call
+// const organizationRouter = require("./src/routers/organization");
+// const eventRouter = require("./src/routers/events")
+// const userRouter = require("./src/routers/user")
+// //const userRouter = require("./api/user/user.router");
+// var corsOptions = {
+//     origin: '*',
+//     optionsSuccessSatus: 200,
+//     enablePreflight: true,
+//     crossDomain: true
+// }
+// app.use(cors(corsOptions))
+
+
+// const port = process.env.port || 3001
+const port = process.env.APP_PORT
+
+app.use(express.json())
+
+// app.use(organizationRouter)
+// app.use(eventRouter)
+// app.use(userRouter)
+
+
+
+
+//app.use('/api/user', userRouter);
+
+
+app.listen(port, () => {
+    console.log("server running on " + port)
+});
