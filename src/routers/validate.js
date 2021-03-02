0ee9860f9ac6fd50a7b408e4efe04b561638d27b
Geoff Nelson <geoffreysnelson@gmail.com>
2021-03-01 16:02:05 -0800

    added config

--- src/routers/validate.js
+++ src/routers/validate.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 validateRouter.get("/validate", cors(), async (req, res) => {
     const validateResp = {
         AUTH_KEY: process.env.AUTH_KEY,
-        AUTH_DOMAIN: process.env.AUTH_DOMAIN
+        AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+        JWTKEY: process.env.JWTKEY
     }
 
     res.status(201).send(validateResp)
