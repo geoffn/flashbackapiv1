dd8a779113bb3443b296ef111483577fb1f71737
Geoff Nelson <geoffreysnelson@gmail.com>
2021-05-11 16:55:45 -0700

    changed auth to Auth

--- src/helpers/token.js
+++ src/helpers/token.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
+    const authHeader = req.headers['Authorization']
   const token = authHeader && authHeader.split(' ')[1]
   console.log(token)
   if (token == null) return res.sendStatus(401)
